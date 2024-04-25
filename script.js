46c42d276ca8bd63ab8a85b42c0ec7f925b6d970
Tchoubi11 <tchoubayefranck@gmail.com>
2024-04-26 00:22:26 +0200

    Déclaration des variables principales dans le fichier script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Déclaration des variables principales à ajouter dans le code
+
+//let score :tableau qui stocke les scores globaux des joueurs
+
+//let roundScore : Un nombre entier qui stocke le score temporaire (ROUND) du joueur actif pendant son tour.
+
+// let activePlayer : Un nombre entier qui indique le joueur actif. Il peut être 1 pour le joueur 1 ou 2 pour le joueur 2.
+
+//let gamePlaying : Un booléen qui indique si le jeu est en cours ou non. Il est utilisé pour contrôler le déroulement du jeu et pour empêcher les actions des joueurs lorsque le jeu est terminé.
+let scores, roundScore, activePlayer, gamePlaying;
+
+
